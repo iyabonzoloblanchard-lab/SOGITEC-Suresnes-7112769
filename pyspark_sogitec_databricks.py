31ba4b9e01459affb5fc63551b949ccef72543df
iyabonzoloblanchard-lab <iyabonzoloblanchard@gmail.com>
2026-09-23 17:54:47 +0200

    Update pyspark_sogitec_databricks.py

--- pyspark_sogitec_databricks.py
+++ pyspark_sogitec_databricks.py
@@ -14,3 +14,22 @@ df_suresnes = df.filter(col("Site") == "Suresnes")
 df_suresnes.show()
 
 print("Projet Data Engineering - Pret pour SOGITEC Suresnes!")
+df.createOrReplaceTempView("simulations")
+
+# Requête 1: Total A350 par site
+resultat1 = spark.sql("""
+    SELECT Site, SUM(Heures_Sim) AS total_heures
+    FROM simulations
+    WHERE Avion = 'A350' AND Site IN ('Suresnes', 'Brest')
+    GROUP BY Site
+""")
+resultat1.show()
+
+# Requête 2: Total par site et avion
+resultat2 = spark.sql("""
+    SELECT Site, Avion, SUM(Heures_Sim) AS tot_h
+    FROM simulations
+    GROUP BY Site, Avion
+    ORDER BY tot_h DESC
+""")
+resultat2.show()
